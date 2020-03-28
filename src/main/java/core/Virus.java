22420d362ee602906831c6e3e2b69e9b0cd4e239
snashka <steelall@yandex.ru>
2020-03-28 03:31:49 +0300

    +multipliers+comments and correct

--- src/main/java/core/Virus.java
+++ src/main/java/core/Virus.java
@@ -56,13 +56,14 @@ public class Virus extends Organism {
         if (o == null || getClass() != o.getClass()) return false;
 
         Virus virus = (Virus) o;
-
+// если  (если имя не равно нулю, тогда имя не равно имени объекта, в противном случае имя объекта неравно нулю) вернуть ложь
         if (name != null ? !name.equals(virus.name) : virus.name != null) return false;
         if (aggression != null ? !aggression.equals(virus.aggression) : virus.aggression != null) return false;
         if (spreadingMethod != null ? !spreadingMethod.equals(virus.spreadingMethod) : virus.spreadingMethod != null)
             return false;
         return mark != null ? mark.equals(virus.mark) : virus.mark == null;
     }
+
     @Override
     public int hashCode() {
         return myHashCode();
@@ -70,33 +71,33 @@ public class Virus extends Organism {
 
     public int myHashCode() {
 
-        String qwerty = "qwertasdfgzxcvbyuiophjklnm";
+        String qwerty = "qwertasdfgzxcvbyuiophjklnm"; //массив - куча для назначения числа символу
         char[] qweArray = new char[qwerty.length()];
         for (int i = 0; i < qweArray.length; i++) {
             qweArray[i] = qwerty.charAt(i);
         }
-        char[] nameArray = new char[name.length()];// name
+        char[] nameArray = new char[name.length()];// name-массив для выбора символа под число
         for (int i = 0; i < nameArray.length; i++) {
             nameArray[i] = name.charAt(i);
         }
 
-        int count = 2;
-        if (nameArray.length>count){
-            count = nameArray.length-2;
+        int count = 2;// число, используемое для выбора буквы из массива поля
+        if (nameArray.length > count) {
+            count = nameArray.length - 2;
         }
-        char temp = nameArray[count];
-        int nameIndex = 0;
+        int nameIndex = 0;// число , используемое для присвоения индекса символа поля из массива -кучи
         for (int i = 0; i < qweArray.length; i++) {
             if (qweArray[i] == nameArray[count]) {
                 nameIndex = i;
             }
         }
+        // далее аналогично первому полю...
         char[] aggrArray = new char[aggression.length()];// aggression
         for (int i = 0; i < aggrArray.length; i++) {
             aggrArray[i] = aggression.charAt(i);
         }
         int aggrIndex = 0;
-            count = aggrArray.length-2;
+        count = aggrArray.length - 2;
         for (int i = 0; i < qweArray.length; i++) {
             if (qweArray[i] == aggrArray[count]) {
                 aggrIndex = i;
@@ -107,7 +108,7 @@ public class Virus extends Organism {
             spreadArray[i] = spreadingMethod.charAt(i);
         }
         int spreadIndex = 0;
-        count = spreadArray.length-2;
+        count = spreadArray.length - 2;
         for (int i = 0; i < qweArray.length; i++) {
             if (qweArray[i] == spreadArray[count]) {
                 spreadIndex = i;
@@ -118,14 +119,14 @@ public class Virus extends Organism {
             markArray[i] = mark.charAt(i);
         }
         int markIndex = 0;
-        count = markArray.length-2;
+        count = markArray.length - 2;
         for (int i = 0; i < qweArray.length; i++) {
             if (qweArray[i] == markArray[count]) {
                 markIndex = i;
             }
         }
 
-        int result = 3*nameIndex + 5*aggrIndex + 7*spreadIndex +13*markIndex;
+        int result = 3 * nameIndex + 5 * aggrIndex + 7 * spreadIndex + 13 * markIndex;
 
         return result;
     }
